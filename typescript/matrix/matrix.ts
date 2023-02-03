e5899c416bbe3a99c4d32d8b01918d3af7fe62c3
Debulois Quentin <quentin@debulois.fr>
2023-02-03 12:26:43 +0100

    Matrix

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,13 +1,26 @@
 export class Matrix {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
+  private _rows: number[][] = []
+  private _columns: number[][] = []
+
+  constructor(matrix: string) {
+    const txtRow = matrix.split("\n")
+    for (let i = 0; i < txtRow.length; i++ ) {
+      this._rows.push(txtRow[i].split(" ").map((x) => { return Number(x) }))
+      for (let j = 0; j < this._rows[i].length; j++ ) {
+        if (this._columns.length < this._rows[i].length) {
+          this._columns.push([this._rows[i][j]])
+        } else {
+          this._columns[j].push(this._rows[i][j])
+        }
+      }
+    }
   }
 
-  get rows(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get rows(): number[][] {
+    return this._rows
   }
 
-  get columns(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get columns(): number[][] {
+    return this._columns
   }
 }
